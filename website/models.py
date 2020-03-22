806a4e91b373b29f4030a059c9bd36840c1bdd38
Pau Agustí Fernández <pau.paf.3@gmail.com>
2020-03-22 18:15:40 +0100

    Changed ClassDiagram Implemented models.py

--- website/models.py
+++ website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField()
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    birth_date = models.DateField()
+    user = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 40)
+    password = models.CharField(max_length = 20)
+
+class Book(models.Model):
+    id = models.IntegerField()
+    ISBN = models.CharField(max_length = 13)
+    title = models.CharField(max_length = 50)
+    cover_page = models.FilePathField(path = '/img')
+    synopsis = models.TextField()
+    release_date = models.DateField()
+
+class Author(models.Model):
+    id = models.IntegerField()
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    birth_date = models.DateField()
+    photo = models.FilePathField(path = '/img')

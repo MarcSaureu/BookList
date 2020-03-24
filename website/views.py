178ae81ba6e54486ebc86e0c77b8ecd0e653703d
Daniel Farre <danifarre97@gmail.com>
2020-03-24 21:40:15 +0100

    There are two users

--- website/views.py
+++ website/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render
+from .models import User
 
 def home(request):
-	return render(request, 'home.html', {})
+	users = User.objects.all()
+	
+	context = {
+		'users' : users
+	}
+
+	return render(request, 'home.html', context)
+
+"""
+from website.models import User                                                         
+
+u1 = User( 
+	first_name='Daniel', 
+	last_name='Farré Serra', 
+	birth_date='1997-10-09', 
+	user_alias='Dani', 
+	email='danifarre97@gmail.com', 
+	password='user' 
+	)                                                                                       
+
+u1.save() 
+"""

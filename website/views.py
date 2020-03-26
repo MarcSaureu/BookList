edbd49ee91293b8c166002b58d55e169e993556f
Marc_Saureu <marcsaureu@gmail.com>
2020-03-26 20:41:54 +0100

    Registration Part Done

--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib import auth
 from .models import User
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
 	return render(request, 'home.html', {})
@@ -11,3 +15,9 @@ def login(request):
 def logout(request):
 	auth.logout(request)
 	return render(request, 'home.html', {})
+
+class RegistroUsuario(CreateView):
+	model = User
+	template_name = "registration/sign_up.html"
+	form_class = UserCreationForm
+	success_url = reverse_lazy('website')

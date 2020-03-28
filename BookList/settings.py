bd42cba72fb20ca18acc87b37d955e8c98a5011f
ahmed <ahmedazzamouri98@gmail.com>
2020-03-28 22:45:10 +0100

    Settings.py and wgsi.py files modificated

--- BookList/settings.py
+++ BookList/settings.py
@@ -120,9 +120,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL =  '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Update database configuration with $DATABASE_URL.
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
+
+# Extra places for collectstatic to find static files
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
+STATICFILES_STORAGE =
+'whitenoise.django.GzipManifestStaticFilesStorage'
